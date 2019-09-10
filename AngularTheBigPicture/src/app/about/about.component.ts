1a70a4d156b77d5a2e39fffd75197919f560cc91
albertmarsnik <albertmarsnik@gmail.com>
2019-09-10 10:23:08 +0200

    Angular_Architecture/Templates _ In line templating was displayed in about component.

--- AngularTheBigPicture/src/app/about/about.component.ts
+++ AngularTheBigPicture/src/app/about/about.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
+  /* This is shown as an example of an in-line template
+  this would then replace the templateUrl attribute
+  template: `<div>
+  <h1>Title</h1>
+  </div>` */
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
